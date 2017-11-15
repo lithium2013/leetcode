3f018fca17c3cf4767a6b7339c30138b1deaef1a
Lithium <379129295@qq.com>
2017-11-15 10:57:56 +0800

    [UPDATE]

--- algorithms/nextPermutation.js
+++ algorithms/nextPermutation.js
@@ -13,7 +13,7 @@ var nextPermutation = function(nums) {
 		for (var j = i + 1; j < len; j++) {
 			if ( nums[j] > nums[i] && (j + 1 >= len || nums[j + 1] <= nums[i]) ) {
 				swap(i, j);
-				reverse(j, len - 1);
+				reverse(i + 1, len - 1);
 				return;
 			}
 		}
